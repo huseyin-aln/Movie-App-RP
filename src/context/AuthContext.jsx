9d30d8cbbeabe729a53773895f2e24f133011e9a
huseyin <huseyinarslan06@gmail.com>
2022-07-19 23:13:24 +0300

    firebase

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(true);
+
+  return (
+    <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
